5e9e55c2005d3b930bb445a3ef4925fc862d8616
Antonina Cherednichenko <antonina.cherednichenko@gmail.com>
2017-06-28 08:30:16 -0700

    change directory structure

--- app/js/controller.js
+++ app/js/controller.js
@@ -10,66 +10,6 @@ var app = angular.module('ethereum-maps-app', []).
 //     apiUrl: ‘http://www.google.com?api’
 // });
 
-app.service('MapUtilsService', function () {
-    var service = this;
-    service.addAllMessagesOnTheMap = function (map) {
-        SmartTrace.getMsgsCount().then(function (data) {
-            let length = data.toNumber();
-            console.log("number of messages = ", length);
-            for (i = 0; i < length; i++) {
-                contract.getMediaMsg(i).then(function (data) {
-                    console.log(`Result for index`, data);
-                    service.addMessageOnMap(data, map);
-                });
-            }
-        });
-    }
-
-    service.addAllSelectedMessagesOnTheMap = function (map, contract) {
-
-        contract.getAllMessages(40, { gas: 500000 }).then(function (data) {
-            let indexArr = data[0];
-            let count = data[1].toNumber();
-            console.log("Running add all selected images with count = ", count);
-
-            for (let i = 0; i < count; i++) {
-                console.log("element", indexArr[i]);
-                contract.getMediaMsg(indexArr[i]).then(function (data) {
-                    service.addMessageOnMap(data, map);
-                });
-            }
-        });
-
-    }
-
-    service.formIPFSLink = function (mediaHash) {
-        return `http://localhost:8080/ipfs/${mediaHash}`;
-    }
-
-    service.addMessageOnMap = function (data, mymap) {
-        console.log("Add message on map");
-        let account = data[0];
-        let mediaHash = data[1];
-        let textHash = data[2];
-        let lat = data[3] / 100000;
-        let long = data[4] / 100000;
-        console.log("account = ", account);
-        console.log("lat = ", data[3], " long = ", data[4]);
-        console.log("lat = ", lat, "long = ", long);
-
-        EmbarkJS.Storage.get(textHash).then(function (messageText) {
-            service.addMarker(lat, long, mediaHash, messageText, mymap);
-        });
-    }
-
-    service.addMarker = function (lat, long, mediaHash, text, mymap) {
-        var marker = L.marker([lat, long]).addTo(mymap);
-        let fullText = `${text} + <a href=${service.formIPFSLink(mediaHash)}>Media</a>`;
-        marker.bindPopup(fullText).openPopup();
-    }
-
-});
-
 
 app.controller("MainController", function ($scope, $window, MapUtilsService) {
     $scope.init = function () {
